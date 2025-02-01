87b857352440f5d8933f804e2badfca0d8cb7527
totmftw <ecom@totm.in>
2025-02-01 20:28:34 +0530

    oo

--- lib/core/config/supabase_config.dart
+++ lib/core/config/supabase_config.dart
@@ -1,5 +1,4 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class SupabaseConfig {
   static const String supabaseUrl = String.fromEnvironment(
@@ -22,7 +21,3 @@ class SupabaseConfig {
     );
   }
 }
-
-final authStateProvider = StreamProvider.autoDispose((ref) {
-  return SupabaseConfig.client.auth.onAuthStateChange.map((event) => event.session);
-});
--- lib/features/auth/providers/auth_provider.dart
+++ lib/features/auth/providers/auth_provider.dart
@@ -1,15 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter_mkd/core/config/supabase_config.dart';
+import 'package:flutter_mkd/features/auth/pages/login_page.dart';
 
-final authProvider = Provider((ref) => AuthProvider());
-
-class AuthProvider {
-  // Define your AuthProvider class here
-}
-
-final supabaseAuthProvider = Provider<GoTrueClient>((ref) {
-  return Supabase.instance.client.auth;
-});
+final supabaseAuthProvider = Provider((ref) => SupabaseConfig.client.auth);
 
 final authStateChangesProvider = StreamProvider<AuthState>((ref) {
   final auth = ref.watch(supabaseAuthProvider);
@@ -25,3 +20,39 @@ final isAuthenticatedProvider = Provider<bool>((ref) {
   final user = ref.watch(currentUserProvider);
   return user != null;
 });
+
+final authStateProvider = StreamProvider.autoDispose((ref) {
+  return SupabaseConfig.client.auth.onAuthStateChange
+      .map((event) => event.session);
+});
+
+class AuthState extends ConsumerStatefulWidget {
+  final Widget child;
+
+  const AuthState({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  @override
+  AuthStateState createState() => AuthStateState();
+}
+
+class AuthStateState extends ConsumerState<AuthState> {
+  @override
+  Widget build(BuildContext context) {
+    final authState = ref.watch(authStateProvider);
+
+    return authState.when(
+      data: (session) {
+        if (session == null) {
+          return const LoginPage();
+        } else {
+          return widget.child;
+        }
+      },
+      loading: () => const CircularProgressIndicator(),
+      error: (error, stack) => Text('Error: $error'),
+    );
+  }
+}

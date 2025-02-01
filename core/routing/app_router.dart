bf41a42b44553cc2169f9dc561afbe5723dff387
totmftw <ecom@totm.in>
2025-02-01 20:13:14 +0530

    Q

--- core/routing/app_router.dart
+++ core/routing/app_router.dart
@@ -1,6 +1,10 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter_mkd/features/auth/pages/login_page.dart';
+import 'core/config/supabase_config.dart';
+import 'core/routing/app_router.dart';
 
 @MaterialAutoRouter(
   replaceInRouteName: 'Page,Route',
@@ -15,4 +19,32 @@ class AppRouter {
   static final _appRouter = $AppRouter();
 
   static AutoRouter get router => _appRouter;
+}
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await SupabaseConfig.initialize();
+
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'MKD App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      routerConfig: AppRouter.router,
+    );
+  }
 }
